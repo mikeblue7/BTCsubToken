7a2b8a617c33c498f2d89cc224bcc210ea6fe4af
Shubham Dusane <sdusane4@gmail.com>
2020-07-27 13:05:54 +0530

    api update

--- contracts/DataOracle.sol
+++ contracts/DataOracle.sol
@@ -3,43 +3,50 @@ import './lib/Oracle/provableAPI_0.5.sol';
 
 contract DataOracle is usingProvable {
     
-    string GET_BTC_PRICE_QUERY = "json(https://api.pro.coinbase.com/products/BTC-USD/ticker).price";
-    string GET_ETH_PRICE_QUERY = "json(https://api.pro.coinbase.com/products/ETH-USD/ticker).price";
-    string GET_BCH_PRICE_QUERY = "json(https://api.pro.coinbase.com/products/BCH-USD/ticker).price";
-    string GET_USD_PRICE_QUERY = "json(https://api.pro.coinbase.com/products/USD/ticker).price";
-    string GET_TUSD_PRICE_QUERY = "json(https://api.pro.coinbase.com/products/TUSD-USD/ticker).price";
-    string GET_BUSD_PRICE_QUERY = "json(https://api.pro.coinbase.com/products/BUSD-USD/ticker).price";
-    string GET_XTZ_PRICE_QUERY = "json(https://api.pro.coinbase.com/products/XTZ-USD/ticker).price";
-    string GET_COMP_PRICE_QUERY = "json(https://api.pro.coinbase.com/products/COMP-USD/ticker).price";
+    string GET_BTC_PRICE_QUERY = "json(https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD).price";
+    string GET_ETH_PRICE_QUERY = "json(https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD).price";
+    string GET_BCH_PRICE_QUERY = "json(https://min-api.cryptocompare.com/data/price?fsym=BCH&tsyms=USD).price";
+    string GET_USD_PRICE_QUERY = "json(https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=USD).price";
+    string GET_TUSD_PRICE_QUERY = "json(https://min-api.cryptocompare.com/data/price?fsym=TUSD&tsyms=USD).price";
+    string GET_BUSD_PRICE_QUERY = "json(https://min-api.cryptocompare.com/data/price?fsym=BUSD&tsyms=USD).price";
+    string GET_XTZ_PRICE_QUERY = "json(https://min-api.cryptocompare.com/data/price?fsym=XTZ&tsyms=USD).price";
+    string GET_COMP_PRICE_QUERY = "json(https://min-api.cryptocompare.com/data/price?fsym=COMP&tsyms=USD).price";
+    string GET_LTC_PRICE_QUERY = "json(https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD).price";
+    string GET_ADA_PRICE_QUERY = "json(https://min-api.cryptocompare.com/data/price?fsym=ADA&tsyms=USD).price";
     
     event LogNewProvableQuery(string description);
     event LogNewProvableResult(string result);
+    event LogConstructorInitiated(string nextStep);
 
     mapping (bytes32 => bool) public pendingQueries;
     string public result;
-        address public owner;
+    address public owner;
+
+
 
     constructor() public payable {
         provable_setProof(proofType_TLSNotary | proofStorage_IPFS);
         owner = msg.sender;
+        emit LogConstructorInitiated("Constructor was initiated. Call 'updatePrice()' to send the Provable Query.");
         provable_setCustomGasPrice(1000000000 wei);
-        
     }
-    
+
     modifier onlyOwner {
         require(owner == msg.sender);
         _;
     }
 
-        function changeOraclizeGasPrice(uint _newGasPrice) external onlyOwner {
+    function changeOraclizeGasPrice(uint _newGasPrice) external onlyOwner {
         provable_setCustomGasPrice(_newGasPrice);
     }
 
     function __callback(bytes32 _myid, string memory _result) public {
         require(msg.sender == provable_cbAddress());
         require (pendingQueries[_myid] == true);
+
         result = _result;
         emit LogNewProvableResult(_result);
+
         delete pendingQueries[_myid]; // This effectively marks the query id as processed.
     }
 
@@ -53,7 +60,7 @@ contract DataOracle is usingProvable {
         }
     }
     
-        function ETHPrice() public payable {
+    function ETHPrice() public payable {
         if (provable_getPrice("URL") > msg.value) {
             revert("Provable query was NOT sent, please add some ETH to cover for the query fee!");
         } else {
@@ -63,7 +70,7 @@ contract DataOracle is usingProvable {
         }
     }
     
-        function BCHPrice() public payable {
+    function BCHPrice() public payable {
         if (provable_getPrice("URL") > msg.value) {
             revert("Provable query was NOT sent, please add some ETH to cover for the query fee!");
         } else {
@@ -73,7 +80,7 @@ contract DataOracle is usingProvable {
         }
     }
     
-        function USDPrice() public payable {
+    function USDPrice() public payable {
         if (provable_getPrice("URL") > msg.value) {
             revert("Provable query was NOT sent, please add some ETH to cover for the query fee!");
         } else {
@@ -84,7 +91,7 @@ contract DataOracle is usingProvable {
     }
     
     
-        function TUSDPrice() public payable {
+    function TUSDPrice() public payable {
         if (provable_getPrice("URL") > msg.value) {
             revert("Provable query was NOT sent, please add some ETH to cover for the query fee!");
         } else {
@@ -94,7 +101,7 @@ contract DataOracle is usingProvable {
         }
     }
     
-        function BUSDPrice() public payable {
+    function BUSDPrice() public payable {
         if (provable_getPrice("URL") > msg.value) {
             revert("Provable query was NOT sent, please add some ETH to cover for the query fee!");
         } else {
@@ -104,7 +111,7 @@ contract DataOracle is usingProvable {
         }
     }
     
-            function XTZPrice() public payable {
+    function XTZPrice() public payable {
         if (provable_getPrice("URL") > msg.value) {
             revert("Provable query was NOT sent, please add some ETH to cover for the query fee!");
         } else {
@@ -114,7 +121,7 @@ contract DataOracle is usingProvable {
         }
     }
     
-                function COMPPrice() public payable {
+    function COMPPrice() public payable {
         if (provable_getPrice("URL") > msg.value) {
             revert("Provable query was NOT sent, please add some ETH to cover for the query fee!");
         } else {
@@ -123,6 +130,25 @@ contract DataOracle is usingProvable {
             pendingQueries[queryId] = true;
         }
     }
+
+    function LTCPrice() public payable {
+        if (provable_getPrice("URL") > msg.value) {
+            revert("Provable query was NOT sent, please add some ETH to cover for the query fee!");
+        } else {
+            emit LogNewProvableQuery("Provable query was sent, standing by for the answer...");
+            bytes32 queryId = provable_query("URL", GET_LTC_PRICE_QUERY);
+            pendingQueries[queryId] = true;
+        }
+    }
     
+        function ADAPrice() public payable {
+        if (provable_getPrice("URL") > msg.value) {
+            revert("Provable query was NOT sent, please add some ETH to cover for the query fee!");
+        } else {
+            emit LogNewProvableQuery("Provable query was sent, standing by for the answer...");
+            bytes32 queryId = provable_query("URL", GET_ADA_PRICE_QUERY);
+            pendingQueries[queryId] = true;
+        }
+    }
     
 }
